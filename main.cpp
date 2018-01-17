712110f5fe79a3a37fe10e917c48ad76a16c717d
tskrzypiec <tomasz.t.skrzypiec@gmail.com>
2018-01-17 12:32:48 +0100

    ver.2

--- main.cpp
+++ main.cpp
@@ -6,11 +6,26 @@ using namespace std;
 int main() {
 
 
-    Rectangle p1;
+    Rectangle p1(3,3);
 
     p1.load();
     p1.calculate();
 
+
+    Rectangle *p2 = new Rectangle(2,5); // dynamicznie przydzielony konstruktor pusty() wiec wezmie 2 i 2
+    //p1.load();
+
+
+    p2->calculate();
+
+
+    Rectangle *p3 = new Rectangle(5,5);  // dobrze
+
+    p3->calculate();
+
+    Rectangle p4 (*p2);  // kopiowanie obiektu p1
+
+    p4.calculate();
 
     return 0;
 }
